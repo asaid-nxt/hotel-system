8d4270cd67ef8c435a94834c1d8752068f769c05
asaid-nxt <asaid@nxt.so>
2024-09-12 14:10:16 +0300

    Implemented enum for role column in users table and added it's tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  enum role: { user: 0, admin: 1 }
+
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,4 +31,35 @@ RSpec.describe User, type: :model do
       expect(user).not_to be_valid
     end
   end
+
+  describe 'Enums' do
+    it 'defines the correct roles' do
+      expect(User.roles.keys).to contain_exactly('user', 'admin')
+    end
+
+    it 'sets default role to user' do
+      user = create(:user)
+      expect(user.role).to eq('user')
+    end
+
+    it 'can set role to admin' do
+      user = create(:user, role: :admin)
+      expect(user.role).to eq('admin')
+    end
+
+    it 'returns true for admin? if the role is admin' do
+      user = create(:user, role: :admin)
+      expect(user.admin?).to be_truthy
+    end
+
+    it 'returns false for admin? if the role is user' do
+      user = create(:user, role: :user)
+      expect(user.admin?).to be_falsey
+    end
+
+    it 'returns true for user? if the role is user' do
+      user = create(:user, role: :user)
+      expect(user.user?).to be_truthy
+    end
+  end
 end
