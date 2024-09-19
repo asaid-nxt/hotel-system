0369ddee9414a038a25715dd7a429ceb85a783b4
asaid-nxt <asaid@nxt.so>
2024-09-19 18:58:28 +0300

    Added integration tests for rooms controller

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -1,15 +1,143 @@
 require 'rails_helper'
 
-RSpec.describe 'Rooms API', type: :request do
+RSpec.describe 'Rooms API', type: :request do # rubocop:disable Metrics/BlockLength
   let(:user) { create(:user) }
+  let(:admin) { create(:user, role: 'admin') }
   let(:hotel) { create(:hotel) }
-  let!(:room) { create(:room, hotel:) }
+  let!(:room) { create(:room, number: '1', hotel:) }
   let(:check_in) { Date.today }
   let(:check_out) { Date.today + 3.days }
-  let(:headers) { { 'Authorization' => jwt_token } }
   let(:jwt_token) { user.generate_jwt }
+  let(:headers) { { 'Authorization' => jwt_token } }
+  let(:admin_headers) { { 'Authorization' => admin.generate_jwt } }
+  let(:valid_attributes) { { number: '1A', capacity: 2, amenities: 'Pool' } }
+  let(:invalid_attributes) { { number: '', amenities: '' } }
 
-  describe 'GET /api/v1/hotels/:hotel_id/rooms/available' do
+  describe 'POST /api/v1/hotels/:hotel_id/rooms' do # rubocop:disable Metrics/BlockLength
+    describe 'when admin is authenticated' do
+      context 'with valid attributes' do
+        before do
+          post "/api/v1/hotels/#{hotel.id}/rooms", params: { room: valid_attributes },
+                                                   headers: admin_headers
+        end
+
+        it 'creates a room' do
+          expect(response).to have_http_status :created
+          expect(json_response['number']).to eq('1A')
+        end
+      end
+
+      context 'with invalid attributes' do
+        before do
+          post "/api/v1/hotels/#{hotel.id}/rooms", params: { room: invalid_attributes },
+                                                   headers: admin_headers
+        end
+
+        it 'returns an error' do
+          expect(response).to have_http_status :unprocessable_entity
+          expect(json_response).to eq({ 'error' => ["Number can't be blank", "Capacity can't be blank",
+                                                    'Capacity is not a number'] })
+        end
+      end
+    end
+
+    describe 'when user is authenticated' do
+      before do
+        post "/api/v1/hotels/#{hotel.id}/rooms", params: { room: valid_attributes },
+                                                 headers:
+      end
+
+      it 'returns Unauthorized error' do
+        expect(response).to have_http_status :unauthorized
+        expect(json_response).to eq({ 'error' => 'Unauthorized' })
+      end
+    end
+
+    describe 'when there is no authentication' do
+      it 'return unauthorized error' do
+        post "/api/v1/hotels/#{hotel.id}/rooms", params: { room: valid_attributes }
+        expect(response).to have_http_status :unauthorized
+        expect(json_response).to eq({ 'error' => 'Unauthorized' })
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/hotels/:hotel_id/rooms/:room_id' do # rubocop:disable Metrics/BlockLength
+    describe 'when room exists' do
+      context 'with valid attributes' do
+        before do
+          put "/api/v1/hotels/#{hotel.id}/rooms/#{room.id}",
+              params: { room: { number: 'updated number' } }, headers: admin_headers
+        end
+
+        it 'updates the room' do
+          expect(response).to have_http_status :ok
+          expect(json_response['number']).to eq('updated number')
+        end
+      end
+
+      context 'with invalid attributes' do
+        before do
+          put "/api/v1/hotels/#{hotel.id}/rooms/#{room.id}",
+              params: { room: { number: '' } }, headers: admin_headers
+        end
+
+        it 'returns an error' do
+          expect(response).to have_http_status :unprocessable_entity
+          expect(json_response).to eq({ 'error' => ["Number can't be blank"] })
+        end
+      end
+    end
+
+    describe "when room doesn't exist" do
+      before do
+        put "/api/v1/hotels/#{hotel.id}/rooms/invalid",
+            params: { room: { number: 'updated number' } }, headers: admin_headers
+      end
+
+      it 'returns not_found error' do
+        expect(response).to have_http_status :not_found
+        expect(json_response).to eq({ 'error' => 'Room not found' })
+      end
+    end
+  end
+
+
+  describe 'DELETE /api/v1/hotels/:hotel_id/rooms/:room_id' do # rubocop:disable Metrics/BlockLength
+    describe 'when room exists' do
+      before do
+        delete "/api/v1/hotels/#{hotel.id}/rooms/#{room.id}", headers: admin_headers
+      end
+
+      it 'deletes the room' do
+        expect(response).to have_http_status :no_content
+      end
+    end
+
+    describe 'when room does not exist' do
+      before do
+        delete "/api/v1/hotels/#{hotel.id}/rooms/invalid", headers: admin_headers
+      end
+
+      it 'return not found error' do
+        expect(response).to have_http_status :not_found
+        expect(json_response).to eq({ 'error' => 'Room not found' })
+      end
+    end
+
+    describe 'when user is not authenticated' do
+      before do
+        delete "/api/v1/hotels/#{hotel.id}/rooms/#{room.id}"
+      end
+
+      it 'return Unauthorized error' do
+        expect(response).to have_http_status :unauthorized
+        expect(json_response).to eq({ 'error' => 'Unauthorized' })
+      end
+    end
+  end
+
+  describe 'GET /api/v1/hotels/:hotel_id/rooms/available' do # rubocop:disable Metrics/BlockLength
     context 'when the user is authenticated and provides valid dates' do
       before do
         get "/api/v1/hotels/#{hotel.id}/rooms/available", params: { hotel_id: hotel.id, check_in:, check_out: },
@@ -48,7 +176,7 @@ RSpec.describe 'Rooms API', type: :request do
       end
     end
 
-    context "when user is not authenticated" do
+    context 'when user is not authenticated' do
       let(:headers) {}
       before do
         get "/api/v1/hotels/#{hotel.id}/rooms/available", params: { hotel_id: hotel.id, check_in:, check_out: },
