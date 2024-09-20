ec493ca00453d0c1fe2bbb224f655f094ae7626d
asaid-nxt <asaid@nxt.so>
2024-09-20 14:05:20 +0300

    Implemented yard docs for hotel and room serializers

--- app/serializers/hotel_serializer.rb
+++ app/serializers/hotel_serializer.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# HotelSerializer is responsible for serializing Hotel objects into JSON format.
+#
+# @!attribute [r] id
+#   @return [Integer] the unique ID of the hotel
+#
+# @!attribute [r] name
+#   @return [String] the name of the hotel
+#
+# @!attribute [r] location
+#   @return [String] the location of the hotel
+#
+# @!attribute [r] amenities
+#   @return [String] the list of amenities available at the hotel
+#
 class HotelSerializer < ActiveModel::Serializer
   attributes :id, :name, :location, :amenities
 end
--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -3,8 +3,8 @@
 # The RoomSerializer class defines how room data is serialized into JSON format.
 # It specifies the attributes to include in the serialized output.
 #
-# @!attribute [r] number
-#   @return [Integer] the room number
+# @!attribute [r] room_number
+#   @return [String] the room number (mapped from the Room model's `number` attribute)
 #
 # @!attribute [r] hotel_id
 #   @return [Integer] the ID of the hotel where the room is located
@@ -13,12 +13,15 @@
 #   @return [Integer] the maximum capacity of the room
 #
 # @!attribute [r] amenities
-#   @return [Array<String>] the amenities available in the room
+#   @return [String] the amenities available in the room
 #
 class RoomSerializer < ActiveModel::Serializer
   # Specifies the attributes to be serialized.
   attributes :room_number, :hotel_id, :capacity, :amenities
 
+  # Maps the room number from the Room model's `number` attribute.
+  #
+  # @return [String] the room number
   def room_number
     object.number
   end
