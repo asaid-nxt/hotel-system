7bb309b2d4c512eb2ec1287a8fcf3ff2ab64104b
asaid-nxt <asaid@nxt.so>
2024-09-20 18:22:52 +0300

    Edited hotel and room serializers to handel images url

--- app/serializers/hotel_serializer.rb
+++ app/serializers/hotel_serializer.rb
@@ -14,6 +14,16 @@
 # @!attribute [r] amenities
 #   @return [String] the list of amenities available at the hotel
 #
+# @!attribute [r] image_url
+#   @return [String, nil] the URL of the hotel's image, or nil if not attached
+#
 class HotelSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :amenities
+  attributes :id, :name, :location, :amenities, :image_url
+
+  # Returns the URL for the hotel's attached image.
+  #
+  # @return [String, nil] The URL of the attached image or nil if no image is attached.
+  def image_url
+    Rails.application.routes.url_helpers.rails_blob_url(object.image, only_path: true) if object.image.attached?
+  end
 end
--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -15,14 +15,24 @@
 # @!attribute [r] amenities
 #   @return [String] the amenities available in the room
 #
+# @!attribute [r] image_url
+#   @return [String, nil] the URL of the room's image, or nil if not attached
+#
 class RoomSerializer < ActiveModel::Serializer
   # Specifies the attributes to be serialized.
-  attributes :room_number, :hotel_id, :capacity, :amenities
+  attributes :room_number, :hotel_id, :capacity, :amenities, :image_url
 
   # Maps the room number from the Room model's `number` attribute.
   #
   # @return [String] the room number
   def room_number
     object.number
+  end
+
+  # Returns the URL for the room's attached image.
+  #
+  # @return [String, nil] The URL of the attached image or nil if no image is attached.
+  def image_url
+    Rails.application.routes.url_helpers.rails_blob_url(object.image, only_path: true) if object.image.attached?
   end
 end
