39d6f5fdcfa7074065b0c4e87ef71ab8b88f5fcf
asaid-nxt <asaid@nxt.so>
2024-09-24 18:51:20 +0300

    Edited user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,9 +4,23 @@
 #
 # This serializer is responsible for specifying the attributes
 # of the User model that will be exposed in the API response.
+#
+# @!attribute [r] avatar_url
+#   @return [String, nil] the URL of the User's avatar, or nil if not attached
+#
 class UserSerializer < ActiveModel::Serializer
   # The attributes of the User model to be included in the serialized JSON.
   #
   # @return [Array<Symbol>] An array of symbols representing the attributes of the user.
-  attributes :id, :username, :role
+  attributes :username, :full_name, :preferences, :image_url
+  # Returns the URL for the User's attached avatar.
+  #
+  # @return [String, nil] The URL of the attached avatar or nil if no image is attached.
+  def image_url
+    Rails.application.routes.url_helpers.rails_blob_url(object.image, only_path: true) if object.image.attached?
+  end
+
+  def full_name
+    "#{object.first_name} #{object.last_name}"
+  end
 end
