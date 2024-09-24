793fb772e402d259ad8b16b0b9e9c1eb98b72e04
asaid-nxt <asaid@nxt.so>
2024-09-24 18:49:13 +0300

    Added first name and last name and prefrences to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_24_130733) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_24_133603) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -75,6 +75,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_24_130733) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "preferences"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
